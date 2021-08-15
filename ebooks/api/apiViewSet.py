9b849e98a8a378f5da87404a8e51f53b8a462772
User <emmanuelbakare@yahoo.com>
2021-08-15 06:50:14 +0100

    'viewset'

--- ebooks/api/apiViewSet.py
+++ ebooks/api/apiViewSet.py
@@ -1,11 +1,11 @@
-    from  rest_framework import viewsets
+from  rest_framework import viewsets
 
-    from ebooks.models import Ebook
-    from ebooks.api.serializer import EbookSerializer
+from ebooks.models import Ebook
+from ebooks.api.serializer import EbookSerializer
 
 
 
-    class EbookViewsets(viewsets.ModelViewSet):
-        
-        queryset= Ebook.objects.all()
-        serializer_class = EbookSerializer
+class EbookViewsets(viewsets.ModelViewSet):
+    
+    queryset= Ebook.objects.all()
+    serializer_class = EbookSerializer
